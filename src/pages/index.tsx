7ada446156f7eb9f798e35d6f65af504a6bfe854
Marcio Flavio <mflaviof1995@gmail.com>
2021-09-04 11:58:31 -0300

    Integracao com a api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import girlCoding from "../../public/images/avatar.svg";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-function Home() {
+type HomeProps = {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+};
+
+function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,9 +27,9 @@ function Home() {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <Image src={girlCoding} alt="Girl coding" />
@@ -29,4 +38,24 @@ function Home() {
   );
 }
 
+const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JVin7ImBFT18N2ytZXQAnwS");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
+
+export { getStaticProps };
 export default Home;
